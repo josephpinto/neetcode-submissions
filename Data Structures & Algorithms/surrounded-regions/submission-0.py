d97932c5ae501aca809f32d82928e71beeea3667
josephpinto <124000783+josephpinto@users.noreply.github.com>
2026-07-10 08:40:59 -0700

    Add: surrounded-regions - submission-0

---
+++ Data Structures & Algorithms/surrounded-regions/submission-0.py
@@ -0,0 +1,48 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+        found_o_set = set()
+        visited = set()
+        directions = [
+            (0,1),
+            (0,-1),
+            (1,0),
+            (-1,0)
+        ]
+        
+        def dfs(r,c):
+            if (r<0 or r >=ROWS or c<0 or c>=COLS
+                or (r,c) in visited):
+                return
+            visited.add((r,c))
+            if board[r][c] == 'X':
+                return
+            # found an O, cannot capture
+            found_o_set.add((r,c))
+
+            for nr,nc in directions:
+                dfs(nr+r,nc+c)
+        
+        border_nodes = []
+        for r in range(ROWS):
+            if board[r][0] == 'O':
+                border_nodes.append((r,0))
+            if board[r][COLS-1] == 'O':
+                border_nodes.append((r,COLS-1))
+        
+        for c in range(COLS):
+            if board[0][c] == 'O':
+                border_nodes.append((0,c))
+            if board[ROWS-1][c] == 'O':
+                border_nodes.append((ROWS-1,c))
+        
+        for r,c in border_nodes:
+            dfs(r,c)
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 'X' or (r,c) in found_o_set:
+                    continue
+                board[r][c] = 'X'
+
+
