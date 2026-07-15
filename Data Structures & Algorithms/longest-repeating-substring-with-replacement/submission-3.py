839ba69d6a234d6e096e40fd251c0dcfe0de5743
josephpinto <124000783+josephpinto@users.noreply.github.com>
2026-07-15 16:41:29 -0700

    Add: longest-repeating-substring-with-replacement - submission-3

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        counts= [0]*26
+
+        l = 0
+        max_length = 0
+        for r, char in enumerate(s):
+            counts[ord(char)-ord('A')] += 1
+
+            while r-l+1 - max(counts) > k:
+                counts[ord(s[l])-ord('A')] -= 1
+                l += 1
+            max_length = max(max_length, r-l+1)
+        return max_length
