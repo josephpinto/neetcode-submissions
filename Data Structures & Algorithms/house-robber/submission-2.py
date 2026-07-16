11815898a35a0bf49f9f33b5d839b87f7b1e49db
josephpinto <124000783+josephpinto@users.noreply.github.com>
2026-07-15 18:09:39 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <=2:
+            return max(nums)
+        first = nums[0]
+        second = max(first,nums[1])
+
+
+        for i in range(2, len(nums)):
+            num = nums[i]
+            curr_sum = max(second, first+num)
+            first,second = second, curr_sum
+        return max(first,second)
