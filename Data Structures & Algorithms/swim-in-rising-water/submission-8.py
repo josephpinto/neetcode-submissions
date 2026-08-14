1d2c476710c7f848b0c4e63d0b0e45beec0c61c2
josephpinto <124000783+josephpinto@users.noreply.github.com>
2026-08-13 21:53:44 -0700

    Add: swim-in-rising-water - submission-8

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-8.py
@@ -0,0 +1,29 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+
+        dirs = [
+            (0,1),
+            (0,-1),
+            (1,0),
+            (-1,0)
+        ]
+
+        heap = [(grid[0][0],0,0)]
+        
+        visit = set()
+        while heap:
+            max_cost, r,c = heapq.heappop(heap)
+            if r == ROWS-1 and c == COLS-1:
+                return max_cost
+            
+            for dr,dc in dirs:
+                nr,nc = r+dr, c+dc
+                if (nr<0 or nr>=ROWS or nc<0 or nc>=COLS or (nr,nc) in visit):
+                    continue
+                visit.add((nr,nc))
+                heapq.heappush(heap,(max(max_cost, grid[nr][nc]),nr,nc))
+
+
+
+
