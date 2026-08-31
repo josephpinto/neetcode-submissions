258512807225dea89beebec9df813af768a1578d
josephpinto <124000783+josephpinto@users.noreply.github.com>
2026-08-31 13:21:20 -0700

    Add: longest-common-subsequence - submission-20

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-20.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # track curr i in text1, curr j in text2
+        # if match, then LCS(i,j) is just 1+LCS(i+1,j+1)
+        # else, try advancing either pointer
+
+        dp = [[0]*(len(text2)+1) for _ in range(len(text1)+1)]
+
+        for i in range(len(text1)-1,-1,-1):
+            for j in range(len(text2)-1,-1,-1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        return dp[0][0]
